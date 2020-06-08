fcf2312cba747cf2c2dfaec6367c060ef411204c
mridx <mridulbaishya28@gmail.com>
2020-06-08 17:59:14 +0530

    added simple material theme

--- app/src/main/java/com/mridx/pdflibrary/LoginActivity.kt
+++ app/src/main/java/com/mridx/pdflibrary/LoginActivity.kt
@@ -13,9 +13,10 @@ class LoginActivity : AppCompatActivity() {
         supportActionBar?.hide()
         create_account.setOnClickListener {
             startActivity(Intent(this, SignupActvity::class.java))
+        }
         user_signIn_btn.setOnClickListener {
             startActivity(Intent(this, DepartmentActivity::class.java))
         }
-        }
+
     }
 }
