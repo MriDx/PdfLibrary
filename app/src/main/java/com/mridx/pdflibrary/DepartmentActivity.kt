3ee1e94fac241abfee929b2909ff16a07db81ccb
NihalP01 <nihalburagohain02@gmail.com>
2020-06-09 13:02:03 +0530

    Added RecyclerView

--- app/src/main/java/com/mridx/pdflibrary/DepartmentActivity.kt
+++ app/src/main/java/com/mridx/pdflibrary/DepartmentActivity.kt
@@ -3,14 +3,24 @@ package com.mridx.pdflibrary
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
-import com.mridx.pdflibrary.R
 import kotlinx.android.synthetic.main.department_recycler.*
 
-class DepartmentActivity: AppCompatActivity() {
+class DepartmentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.department_recycler)
-       department_recycler_view.layoutManager = GridLayoutManager(this,2)
+        val departmentList = arrayList(200)
+        department_recycler_view.layoutManager = GridLayoutManager(this, 2)
+        department_recycler_view.adapter =
+            DepartmentAdapter(departmentList as ArrayList<DepartmentData>)
         department_recycler_view.setHasFixedSize(true)
+    }
+
+    private fun arrayList(size: Int): List<DepartmentData> {
+        val list = ArrayList<DepartmentData>()
+        for (i in 0 until size) {
+            list.add(DepartmentData("Department $i"))
+        }
+        return list
     }
 }
