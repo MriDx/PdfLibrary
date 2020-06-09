9acb6a3afb15f827be06910de3ff2b31540cfcea
NihalP01 <nihalburagohain02@gmail.com>
2020-06-09 16:04:09 +0530

    Changed the layouts to ConstraintLayout

--- app/src/main/java/com/mridx/pdflibrary/SignupActvity.kt
+++ app/src/main/java/com/mridx/pdflibrary/SignupActvity.kt
@@ -3,13 +3,13 @@ package com.mridx.pdflibrary
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_signup.*
+import kotlinx.android.synthetic.main.signup_ui.*
 
 class SignupActvity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signup)
+        setContentView(R.layout.signup_ui)
         exist_account_SignIn.setOnClickListener {
             finish()
         }
