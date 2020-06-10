29493f548e1abccbe2a2be5141c75ffc5191dec1
NihalP01 <nihalburagohain02@gmail.com>
2020-06-11 00:26:06 +0530

    Added material theme

--- app/src/main/java/com/mridx/pdflibrary/LoginActivity.kt
+++ app/src/main/java/com/mridx/pdflibrary/LoginActivity.kt
@@ -1,22 +1,23 @@
 package com.mridx.pdflibrary
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.mridx.pdflibrary.UI.Recycler.DepartmentActivity
 import kotlinx.android.synthetic.main.login_ui.*
+
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_ui)
-        create_account.setOnClickListener {
-            startActivity(Intent(this, SignupActvity::class.java))
-        }
-        user_signIn_btn.setOnClickListener {
-            startActivity(Intent(this, DepartmentActivity::class.java))
-        }
+         create_account.setOnClickListener {
+             startActivity(Intent(this, SignupActvity::class.java))
+         }
+         user_signIn_btn.setOnClickListener {
+             startActivity(Intent(this, DepartmentActivity::class.java))
+         }
 
     }
 }
