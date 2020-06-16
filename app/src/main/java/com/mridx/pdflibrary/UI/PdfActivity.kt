4b52153eb1a5deb12368f61faa5a162e82f20155
NihalP01 <nihalburagohain02@gmail.com>
2020-06-16 20:06:19 +0530

    Added PDF viewer

--- app/src/main/java/com/mridx/pdflibrary/UI/PdfActivity.kt
+++ app/src/main/java/com/mridx/pdflibrary/UI/PdfActivity.kt
@@ -5,11 +5,22 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.mridx.pdflibrary.R
+import kotlinx.android.synthetic.main.pdfview.*
 
 class PdfActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.pdfview)
-
+        pdfView.fromAsset("sample.pdf")
+            .enableSwipe(true)
+            .swipeHorizontal(false)
+            .enableDoubletap(true)
+            .defaultPage(0)
+            .enableAnnotationRendering(false)
+            .password(null)
+            .scrollHandle(null)
+            .enableAntialiasing(true)
+            .spacing(0)
+            .load();
     }
 }
